3853c7afdf23d1c57581ea9eb3cbf08061127978
mauriceweber <webermau@inf.ethz.ch>
2022-03-27 18:01:44 +0200

    transforms eval scripts

---
+++ run_eval_contrast.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+DATA_ROOT="/mnt/ds3lab-scratch/webermau/rey-figure/data/serialized-data/data-2018-2021-232x300-pp0"
+RES_DIR="/home/webermau/rey-figure/spaceml-results/data-2018-2021-232x300-pp0/final-bigsize-aug/rey-multilabel-classifier"
+
+for contrast in $(seq 0.1 0.1 2.0); do
+  python eval_multilabel_semantic.py --data-root "$DATA_ROOT" \
+    --results-dir "$RES_DIR" \
+    --image-size 232 300 \
+    --transform "contrast" \
+    --contrast $contrast
+done
--- run_eval_rotations.sh
+++ run_eval_rotations.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-DATA_ROOT=""
-RES_DIR=""
+DATA_ROOT="/mnt/ds3lab-scratch/webermau/rey-figure/data/serialized-data/data-2018-2021-232x300-pp0"
+RES_DIR="/home/webermau/rey-figure/spaceml-results/data-2018-2021-232x300-pp0/final-bigsize-aug/rey-multilabel-classifier"
 
 angle=0
 while [ $angle -le 30 ]; do
