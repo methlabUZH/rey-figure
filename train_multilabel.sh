d28c79dde1ddef293aba46bb64522c7d5f871c7e
Lukas Wolf <wolf.lukas@mailbox.org>
2022-03-30 09:37:10 +0000

    fix nohup

--- train_multilabel.sh
+++ train_multilabel.sh
@@ -5,13 +5,13 @@ date=$(date +%y-%m-%d-%H-%M)
 
 nohup \
 python train_multilabel.py \
-    --data-root ./data/serialized-data/data-2018-2021-116x150-pp0 \
+    --data-root ./data/serialized-data/data_116x150-seed_1 \
     --results-dir ./results \
     --eval-test \
-    --epochs 75 \
+    --epochs 30 \
     --batch-size 64 \
     --lr 0.01 \
     --gamma 0.95 \
     --weighted-sampling 1 \
-    --image-size 116 150
+    --image-size 116 150 \
 > "./log/$date$_console.out" & 
